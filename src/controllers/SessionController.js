c0a3db571f5d0bacc1b0245a42556711dbf8fb96
Lucas Sodre <lucasmeinen@yahoo.com.br>
2023-10-05 16:09:44 -0300

    Validação verifica se usario já existe

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -12,7 +12,13 @@ class SessionController {
 
     const { email } = req.body
 
-    let user = await User.create({ email })
+    //Verificando se o usuario já existe na aplicação
+    let user = await User.findOne({ email })
+
+    if(!user){
+      user = await User.create({ email })
+
+    }
 
     return res.json(user);
   }
